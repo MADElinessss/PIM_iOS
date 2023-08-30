83dbc85f1b9ee3335e12bbed34d4f0fe4817d6ef
MADElinessss <jyseen@naver.com>
2023-08-30 23:17:28 +0900

    프로젝트 생성

---
+++ PIM_Pill_Reminder/PIM_Pill_Reminder/PIM_Pill_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIM_Pill_ReminderApp.swift
+//  PIM_Pill_Reminder
+//
+//  Created by 신정연 on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct PIM_Pill_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
